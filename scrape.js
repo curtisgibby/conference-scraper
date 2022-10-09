a89e686ffdc3df915c7a3bc35cec711297fafec4
Curtis Gibby <cgibby@gmail.com>
2022-10-09 13:53:28 -0600

    Fix minor whitespace

--- scrape.js
+++ scrape.js
@@ -26,11 +26,11 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
  */
 async function loadSavedCredentialsIfExist() {
 	try {
-	const content = await fs.readFile(TOKEN_PATH);
-	const credentials = JSON.parse(content);
-	return google.auth.fromJSON(credentials);
+		const content = await fs.readFile(TOKEN_PATH);
+		const credentials = JSON.parse(content);
+		return google.auth.fromJSON(credentials);
 	} catch (err) {
-	return null;
+		return null;
 	}
 }
 
@@ -45,10 +45,10 @@ async function saveCredentials(client) {
 	const keys = JSON.parse(content);
 	const key = keys.installed || keys.web;
 	const payload = JSON.stringify({
-	type: 'authorized_user',
-	client_id: key.client_id,
-	client_secret: key.client_secret,
-	refresh_token: client.credentials.refresh_token,
+		type: 'authorized_user',
+		client_id: key.client_id,
+		client_secret: key.client_secret,
+		refresh_token: client.credentials.refresh_token,
 	});
 	await fs.writeFile(TOKEN_PATH, payload);
 }
